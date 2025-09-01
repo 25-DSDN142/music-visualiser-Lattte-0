348ea64df8fef967c0b3731ac394887dde643b67
Lattte-0 <clarkchar3@myvuw.ac.nz>
2025-09-02 11:48:18 +1200

    yrxmj

--- music_starter.js
+++ music_starter.js
@@ -1,43 +1,42 @@
 
+let sunY =100
+let sunSize;
+let bgcol
+let moonSize;
+let moonY =60
+
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(20)
-  textFont('Verdana'); // please use CSS safe fonts
-  rectMode(CENTER)
-  textSize(24);
   
-   let bar_spacing = height / 10;
-   let bar_height = width / 12;
-   let bar_pos_x = width / 2;
+  frameRate(5);
+
+  background(48, 25, 52	,bgcol)//bgcol goes here
+  moonSize= map(drum, 0, 100, 50, 100)
+  bgCol = map(0, 100, 100, 255)
+
+fill(72, 50, 72)
+rect(0, 300, 1589,); //Sea
+
+fill(216, 191, 216)
+circle(700,moonSize,moonY)//Moon
+
+
+  //fill(66, 135, 245) //blue
+  //ellipse (300, sunY, sunSize)
+  //sunY = sunY +1
+
+
+  //for(var i= 1; i< 6 ;i++){
+ //var circleStep =i=800
+ //ellipse (1000, sunY+(150*i), sunSize) //circleStep
+ //sunY = sunY +1
+
+  }
+
+  if(sunY >1000) {
+sunY=0
+   
+  }
  
-// changes 
-   // vocal bar is red
-   fill(200, 0, 0);
-   rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-   fill(0);
-   text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
- 
-   // drum bar is green
-   fill(0, 200, 0);
-   rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-   fill(0);
-   text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
- 
-   // bass bar is blue
-   fill(50, 50, 240);
-   rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-   fill(0);
-   text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-   // other bar is white
-   fill(200, 200, 200);
-   rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-   fill(0);
-   text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-   fill(255, 255, 0);
- 
-   // display "words"
-   textAlign(CENTER);
-   textSize(vocal);
-   text(words, width/2, height/3);
-}
+
+//}
