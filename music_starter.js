a7694469a243ca6637eae01b55dc6a411058fe56
Lattte-0 <clarkchar3@myvuw.ac.nz>
2025-09-09 11:46:18 +1200

    added image yippe

--- music_starter.js
+++ music_starter.js
@@ -4,23 +4,33 @@ let sunSize;
 let bgcol
 let moonSize;
 let moonY =60
+let firstRun = true;
+let myImage;
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
   
+  if (firstRun){
+
+  myImage =loadImage('gloriaSprite.png')
+  firstRun = false;
+
+}
+  
   frameRate(5);
 
-  background(48, 25, 52	,bgcol)//bgcol goes here
+  background(66,68,113,bgcol)//bgcol goes here
   moonSize= map(drum, 0, 100, 50, 100)
   bgCol = map(0, 100, 100, 255)
 
-fill(62, 30, 66)
+fill(104,106,138)
 rect(0, 350, 1589,)//sky
 
-triangle(80, 205, 108, 70, 136, 205); //mountain 2
+fill(91,92,120) //Mountain colours
+triangle(0, 1000, 200, 40,806, 600); //mountain 2
 triangle(-95, 500, -108, 70, 536, 500); //mountain 1
 
-fill(72, 50, 72)
+fill(112,120,91)
 rect(0, 450, 1589,); //grass patch
 
 
@@ -28,7 +38,12 @@ fill(216, 191, 216)
 circle(700,moonSize,moonY)//Moon
 
 
-  //fill(66, 135, 245) //blue
+
+
+image(myImage, 400, 400);
+
+
+//fill(66, 135, 245) //blue
   //ellipse (300, sunY, sunSize)
   //sunY = sunY +1
 
